9c5c601fd9753c8cb2e4e9a02f57123565f4f2c9
Chalel <charlie03.pl@gmail.com>
2024-02-07 16:16:42 +0100

    basics

---
+++ Todo.ts
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const addButton = document.getElementById('addTask') as HTMLButtonElement;
+    const todoInput = document.getElementById('task') as HTMLInputElement;
+    const todoList = document.getElementById('toDoList') as HTMLUListElement;
+})
