1f5d0b5410a8f1e40a0489eb6a035d9e323ee5ec
Chalel <charlie03.pl@gmail.com>
2024-02-08 11:26:26 +0100

    Dags att bygga om typescript :/

--- todo.js
+++ todo.js
@@ -1,59 +1,46 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const addButton = document.getElementById('addTask') as HTMLButtonElement;
-    const todoInput = document.getElementById('task') as HTMLInputElement;
-    const todoList = document.getElementById('toDoList') as HTMLUListElement;
-
-    const addTask = () => {
-        const taskValue = todoInput.value.trim();
+window.addEventListener('DOMContentLoaded', function (event) {
+    var addButton = document.getElementById('addTask');
+    var todoInput = document.getElementById('task');
+    var todoList = document.getElementById('toDoList');
+    var addTask = function () {
+        var taskValue = todoInput.value.trim();
         if (!taskValue) {
             alert("Please enter a task!");
             return;
         }
-
-        const card = document.createElement('div');
+        var card = document.createElement('div');
         card.className = 'card';
         // card.id = uniqueId();
-
-        const checkBox = document.createElement('input');
+        var checkBox = document.createElement('input');
         checkBox.className = 'check';
         checkBox.type = 'checkbox';
-
-        const todoTitle = document.createElement('p');
+        var todoTitle = document.createElement('p');
         todoTitle.className = 'todoTitle';
         todoTitle.textContent = taskValue;
-
-        const trashCan = document.createElement('img');
+        var trashCan = document.createElement('img');
         trashCan.src = "remove.png";
-        trashCan.className = 'trashCan'
+        trashCan.className = 'trashCan';
         trashCan.width = 30;
         trashCan.height = 30;
         trashCan.addEventListener('click', function () {
             card.remove();
         });
-
         card.appendChild(checkBox);
         card.appendChild(todoTitle);
         card.appendChild(trashCan);
-
         todoList.appendChild(card);
-    }
-
+    };
     addButton.addEventListener('click', addTask);
-
-    todoInput.addEventListener('keypress', (e) => {
+    todoInput.addEventListener('keypress', function (e) {
         if (e.key === 'Enter') {
             addTask();
         }
     });
 });
-
 // function uniqueId(): string {
 //     const timestamp = Date.now();
 //     const randomComponent = Math.floor(Math.random() * 99999) + 1
-
 //     const uniqueId = `${timestamp}${randomComponent.toString().padStart(5, '0')}`;
-
 //     return uniqueId;
 // };
-
 //HONESTLY REDO HELA JÄVLA SKITEN
