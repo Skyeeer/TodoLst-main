d56de22a3ab3e59e3e44210610639b1bc5bd4fe4
Chalel <charlie03.pl@gmail.com>
2024-02-16 14:33:17 +0100

    Lagt till flera funktionaliteter Gjort klart

--- Todo.ts
+++ Todo.ts
@@ -1,32 +1,123 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadTodos();
+    ListVis();
+    ClearAllEvent();
+});
 const inputField = document.getElementById('task') as HTMLInputElement;
 const addBtn = document.getElementById('addTask') as HTMLInputElement;
-const toDoList = document.getElementById('toDoList') as HTMLInputElement;
+const toDoList = document.getElementById('toDoList') as HTMLUListElement;
+let todos: { id: string; content: string; completed: boolean; }[] = [];
+
 
 if (addBtn && inputField && toDoList) {
     addBtn.addEventListener('click', () => {
         const toDoValue = inputField.value.trim();
 
         if (toDoValue !== '') {
-            const toDoCard = `<li class="list-Item"> <div class="card"><input class="check" type="checkbox">
-                <p class="todoTitle">${toDoValue}</p> <img src="remove.png" class="trashCan" width="30"
-                    height="30">
-            </div>
-            </li>`;
-
-
-            const newToDo = document.createElement('li');
-            newToDo.innerHTML = toDoCard;
+            const todoId = generateId();
+            const toDoCard = createTodoCard(toDoValue, todoId, false);
+            toDoList.innerHTML += toDoCard;
+            todos.push({ id: todoId, content: toDoValue, completed: false });
+            saveTodos();
             inputField.value = '';
-            newToDo.querySelector('.trashCan')?.addEventListener('click', function (this: HTMLImageElement) {
-                const listItem = this.closest('li');
-                if (listItem !== null) {
-                    listItem.remove();
-                }
-            });
+            ListVis();
         } else {
             alert('Please enter a to-do item.');
         }
 
-
+        // ClearAll();
     });
+    toDoList.addEventListener('click', function (event) {
+        if ((event.target as HTMLElement).classList.contains('trashCan')) {
+            const listItem = (event.target as HTMLElement).closest('li');
+            if (listItem !== null) {
+                const todoId = listItem.id;
+                todos = todos.filter(todo => todo.id !== todoId);
+                saveTodos();
+                listItem.remove();
+                ListVis();
+            }
+        }
+    });
+
+    toDoList.addEventListener('change', function (event) {
+        const target = event.target as HTMLInputElement;
+        if (target.classList.contains('check')) {
+            const card = target.closest('.card');
+            if (card) {
+                const listItem = card.closest('li');
+                const todoId = listItem?.id;
+                const todo = todos.find(todo => todo.id === todoId);
+                if (todo) {
+                    todo.completed = target.checked;
+                    saveTodos();
+                }
+                const text = card.querySelector('.todoTitle');
+                if (text) {
+                    text.classList.toggle('crossed-out', target.checked);
+                }
+            }
+        }
+    });
+
 }
+//Gömmer Clear All knappen
+function ListVis() {
+    const listContainer = document.querySelector('.List') as HTMLElement;
+    if (listContainer) {
+        listContainer.style.display = toDoList.children.length > 0 ? 'block' : 'none';
+    }
+}
+
+//Tar bort alla Todos
+function ClearAll() {
+    const clearAllBtn = document.getElementById('clear') as HTMLButtonElement;
+
+    if (clearAllBtn) {
+        clearAllBtn.addEventListener('click', () => {
+            toDoList.innerHTML = '';
+            todos = [];
+            ListVis();
+        })
+    }
+}
+
+function ClearAllEvent() {
+    const clearAllBtn = document.getElementById('clear') as HTMLButtonElement;
+    if (clearAllBtn) {
+        clearAllBtn.addEventListener('click', ClearAll);
+    }
+}
+
+function createTodoCard(toDoValue: string, todoId: string, completed: boolean): string {
+    const crossedOut = completed ? 'crossed-out' : '';
+    return `<li class="list-Item" id="${todoId}"> <div class="card"><input class="check" type="checkbox" ${completed ? 'checked' : ''}>
+                <p contentEditable class="todoTitle ${crossedOut}">${toDoValue}</p> <img src="remove.png" class="trashCan" width="30"
+                    height="30">
+            </div>
+            </li>`;
+}
+
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function generateId() {
+    const time = new Date().getTime();
+    const random = Math.floor(Math.random() * 1000);
+    return `${time}${random}`;
+}
+
+function loadTodos(): void {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+        todos = JSON.parse(storedTodos);
+        todos.forEach(todo => {
+            const toDoCard = createTodoCard(todo.content, todo.id, todo.completed);
+            toDoList.innerHTML += toDoCard;
+        });
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', ListVis);
--- todo.js
+++ todo.js
@@ -1,29 +1,110 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    loadTodos();
+    ListVis();
+    ClearAllEvent();
+});
 const inputField = document.getElementById('task');
 const addBtn = document.getElementById('addTask');
 const toDoList = document.getElementById('toDoList');
+let todos = [];
 if (addBtn && inputField && toDoList) {
     addBtn.addEventListener('click', () => {
-        var _a;
         const toDoValue = inputField.value.trim();
         if (toDoValue !== '') {
-            const toDoCard = `<li class="list-Item"> <div class="card"><input class="check" type="checkbox">
-                <p class="todoTitle">${toDoValue}</p> <img src="remove.png" class="trashCan" width="30"
-                    height="30">
-            </div>
-            </li>`;
-            const newToDo = document.createElement('li');
-            newToDo.innerHTML = toDoCard;
+            const todoId = generateId();
+            const toDoCard = createTodoCard(toDoValue, todoId, false);
+            toDoList.innerHTML += toDoCard;
+            todos.push({ id: todoId, content: toDoValue, completed: false });
+            saveTodos();
             inputField.value = '';
-            (_a = newToDo.querySelector('.trashCan')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
-                const listItem = this.closest('li');
-                if (listItem !== null) {
-                    listItem.remove();
-                }
-            });
+            ListVis();
         }
         else {
             alert('Please enter a to-do item.');
         }
+        // ClearAll();
+    });
+    toDoList.addEventListener('click', function (event) {
+        if (event.target.classList.contains('trashCan')) {
+            const listItem = event.target.closest('li');
+            if (listItem !== null) {
+                const todoId = listItem.id;
+                todos = todos.filter(todo => todo.id !== todoId);
+                saveTodos();
+                listItem.remove();
+                ListVis();
+            }
+        }
+    });
+    toDoList.addEventListener('change', function (event) {
+        const target = event.target;
+        if (target.classList.contains('check')) {
+            const card = target.closest('.card');
+            if (card) {
+                const listItem = card.closest('li');
+                const todoId = listItem === null || listItem === void 0 ? void 0 : listItem.id;
+                const todo = todos.find(todo => todo.id === todoId);
+                if (todo) {
+                    todo.completed = target.checked;
+                    saveTodos();
+                }
+                const text = card.querySelector('.todoTitle');
+                if (text) {
+                    text.classList.toggle('crossed-out', target.checked);
+                }
+            }
+        }
     });
 }
+//Gömmer Clear All knappen
+function ListVis() {
+    const listContainer = document.querySelector('.List');
+    if (listContainer) {
+        listContainer.style.display = toDoList.children.length > 0 ? 'block' : 'none';
+    }
+}
+//Tar bort alla Todos
+function ClearAll() {
+    const clearAllBtn = document.getElementById('clear');
+    if (clearAllBtn) {
+        clearAllBtn.addEventListener('click', () => {
+            toDoList.innerHTML = '';
+            todos = [];
+            ListVis();
+        });
+    }
+}
+function ClearAllEvent() {
+    const clearAllBtn = document.getElementById('clear');
+    if (clearAllBtn) {
+        clearAllBtn.addEventListener('click', ClearAll);
+    }
+}
+function createTodoCard(toDoValue, todoId, completed) {
+    const crossedOut = completed ? 'crossed-out' : '';
+    return `<li class="list-Item" id="${todoId}"> <div class="card"><input class="check" type="checkbox" ${completed ? 'checked' : ''}>
+                <p contentEditable class="todoTitle ${crossedOut}">${toDoValue}</p> <img src="remove.png" class="trashCan" width="30"
+                    height="30">
+            </div>
+            </li>`;
+}
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function generateId() {
+    const time = new Date().getTime();
+    const random = Math.floor(Math.random() * 1000);
+    return `${time}${random}`;
+}
+function loadTodos() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+        todos = JSON.parse(storedTodos);
+        todos.forEach(todo => {
+            const toDoCard = createTodoCard(todo.content, todo.id, todo.completed);
+            toDoList.innerHTML += toDoCard;
+        });
+    }
+}
+document.addEventListener('DOMContentLoaded', ListVis);
